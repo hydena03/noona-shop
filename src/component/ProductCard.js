9d63a123d6f91a33bcc005f46c07649edd3724d0
kyeongrok <hydena03@gmail.com>
2024-03-28 23:21:43 +0900

    hello

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 
-const ProductCard = () => {
+const productCard = ({item}) => {
   return (
     <div>
-        <img src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F07%2F13%2F071335db362f84c820989a92c97124d1500e51f0.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]"/>
-        <div>타이틀</div>
-        <div>상품명</div>
-        <div>가격</div>
-        <div>신제품</div>
+         <img src={item?.img} alt="H&M 로고"/>
+        <div>{item.choice === true?"Conscious choice" : " "}</div>
+        <div>{item?.title}</div>
+        <div>{item?.price}</div>
+        <div>{item?.new === true?"신제품" : " "}</div>
     </div>
 
   )
 }
 
-export default ProductCard
+export default productCard
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,21 +1,36 @@
-import React, {useEffect, useState} from 'react'
+
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col} from "react-bootstrap";
+import ProductCard from '../component/productCard';
+
 
 const ProductAll = () => {
-    const[ProductList, setProductList] = useState([]);
-const getProducts = async ()=>{
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async () => {
     let url =`http://localhost:5000/products`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
-}
-    useEffect(()=>{
-getProducts()
-    },[])
-  return( 
-  <div>
+  };
 
+  useEffect (() => {
+    getProducts()
+  },[])
+
+  return (
+    <div>
+ <Container>
+ <Row>
+  {productList.map((menu)=> (
+  <Col lg={3}>
+  <ProductCard item={menu} />
+  </Col>
+  ))}
+</Row>
+      </Container>
     </div>
   )
-}
+  }
 
 export default ProductAll
