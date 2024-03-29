be242429ec32ad2c09ea2bef44f4f62acbfbbb44
kyeongrok <hydena03@gmail.com>
2024-03-29 21:18:55 +0900

    first commit

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,38 +1,36 @@
-
-import React, { useEffect, useState } from 'react'
-import { Container, Row, Col} from "react-bootstrap";
-import ProductCard from '../component/productCard.js';
-
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import ProductCard from '../component/ProductCard'; // 파일명을 정확하게 대소문자 일치시켜 import
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
 
-  const getProducts = async() => {
-    let url =  `http://localhost:5000/products/`
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products/`;
     // let url = `http://my-json-server.typicode.com/hydena03/H-M-react/products/${id}`
     let response = await fetch(url);
     let data = await response.json();
     
     setProductList(data);
-  }
+  };
 
-  useEffect (() => {
-    getProducts()
-  },[])
+  useEffect(() => {
+    getProducts();
+  }, []);
 
   return (
     <div>
- <Container>
- <Row>
-  {productList.map((menu)=> (
-  <Col lg={3}>
-  <ProductCard item={menu} />
-  </Col>
-  ))}
-</Row>
+      <Container>
+        <Row>
+          {productList.map((menu) => (
+            <Col lg={3}>
+              <ProductCard item={menu} />
+            </Col>
+          ))}
+        </Row>
       </Container>
     </div>
-  )
-  }
+  );
+};
 
-export default ProductAll
+export default ProductAll;
